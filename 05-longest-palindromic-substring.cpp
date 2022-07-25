85bfb095648afb805666509fc7bbe6aa6810421a
gosha20777 <gosha20777@live.ru>
2022-07-25 18:11:54 +0300

    add task 5

---
+++ 05-longest-palindromic-substring.cpp
@@ -0,0 +1,20 @@
+//
+// Created by gosha20777 on 7/25/22.
+//
+
+
+#include "iostream"
+#include "vector"
+#include "set"
+
+using namespace std;
+
+
+int main(){
+    string str = "bb";
+
+    Solution s;
+    auto result = s.longestPalindrome(str);
+    cout << result << endl;
+    return 0;
+}
